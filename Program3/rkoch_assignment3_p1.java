2728ce424e141cff0043041197ed815f5f30a2c8
raytracer <ray3618@gmail.com>
2022-10-28 20:32:20 -0600

    close the results file!

--- Program3/rkoch_assignment3_p1.java
+++ Program3/rkoch_assignment3_p1.java
@@ -118,5 +118,6 @@ public class rkoch3_p1 {
 	// prints the solution matrix to a given file
 	public static void printSolutionToFile() throws IOException {
 		resultsFile.printf("%.4f\n%.4f", solution[0][0], solution[1][0]);
+		resultsFile.close();
 	}
 }
