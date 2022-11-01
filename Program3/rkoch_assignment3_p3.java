433580b752bd6815648aeb152f8cac6ef073486e
raytracer <ray3618@gmail.com>
2022-10-31 18:17:10 -0600

    added input file method

---
+++ Program3/rkoch_assignment3_p3.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.math.*;
+
+public class rkoch_assignment3_p3 {
+	public static File inputFile;
+
+	public static void main(String[] args) throws IOException {
+
+		// setup the input file and count the number of lines in it
+		setupTheInputFile();
+	}
+
+	// prompts the user for an input file and returns it
+	public static void setupTheInputFile() throws IOException {
+		Scanner userInput = new Scanner(System.in);
+		System.out.printf("Please enter the name of an input file: ");
+		inputFile = new File(userInput.nextLine());
+	}
+
+}
