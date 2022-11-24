48bbd489e30f9e9a88f6eeba0e921e274f47a69a
raytracer <ray3618@gmail.com>
2022-11-24 10:21:48 -0700

    created file, added dependencies and main

---
+++ Program4/rkoch_assignment4_p1.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class rkoch_assignment4_p1 {
+	public static void main(String[] args) throws IOException {
+
+	}
+}
