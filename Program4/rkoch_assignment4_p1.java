9c02095471747bdcca6aa6d1559076ea59f51dbd
raytracer <ray3618@gmail.com>
2022-11-25 13:12:13 -0700

    added printing and sig fig rounding

--- Program4/rkoch_assignment4_p1.java
+++ Program4/rkoch_assignment4_p1.java
@@ -2,6 +2,8 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
+import java.math.BigDecimal;
+import java.math.MathContext;
 
 public class rkoch_assignment4_p1 {
 
@@ -43,5 +45,26 @@ public class rkoch_assignment4_p1 {
 			filename = userInput.nextLine();
 		}
 		resultsFile = new PrintWriter(new File(filename));
+	}
+
+	// rounds a double to a specified number of significant digits
+	public static double roundToSignificantDigits(double number, int digits) {
+		double roundedNum;
+
+		BigDecimal bigDecimal = new BigDecimal(number);
+		bigDecimal = bigDecimal.round(new MathContext(digits));
+		roundedNum = bigDecimal.doubleValue();
+
+		return roundedNum;
+	}
+
+	// prints a matrix to a given file
+	public static void printMatrixToFile(double[][] matrix) throws IOException {
+		for (int row = 0; row < matrix.length; row++) {
+			for (int column = 0; column < matrix[row].length; column++) {
+				resultsFile.printf("%-8.4g", roundToSignificantDigits(matrix[row][column], 4));
+			}
+			resultsFile.println();
+		}
 	}
 }
