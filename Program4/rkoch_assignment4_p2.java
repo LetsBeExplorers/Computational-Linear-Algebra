804f6c3032af8de021dc5307377e27324b81eed3
raytracer <ray3618@gmail.com>
2022-11-27 18:09:09 -0700

    removed unnecessary vector printing

--- Program4/rkoch_assignment4_p2.java
+++ Program4/rkoch_assignment4_p2.java
@@ -12,6 +12,8 @@ public class rkoch_assignment4_p2 {
 	public static final double tolerance = .000001;
 	public static ArrayList<Vector> points = new ArrayList<Vector>();
 	public static ArrayList<Plane> planes = new ArrayList<Plane>();
+	public static ArrayList<Triangle> triangles = new ArrayList<Triangle>();
+	public static Line line;
 	public static File inputFile;
 	public static PrintWriter resultsFile1;
 	public static PrintWriter resultsFile2;
@@ -27,12 +29,20 @@ public class rkoch_assignment4_p2 {
 		if(args.length > 2) filename1 = args[2];
 		setupTheOutputFile(filename, filename1);
 
-		// handle the input routing to create planes and points
+		// handle the input routing to create planes and points for part 1
 		part1InputHandler();
 
 		// for each point and plane, calculate the distance and print it for part 1
 		for (int i = 0; i < planes.size(); i++) {
 			printNumberToFile(planes.get(i).distanceTo(points.get(0)), resultsFile1);
+		}
+
+		// handle the input routing to create a line and triangles for part 2
+		part2InputHandler();
+
+		// for each triangle, compute the intersection point with the line
+		for (int i = 0; i < triangles.size(); i++) {
+
 		}
 
 		resultsFile1.close();
@@ -71,7 +81,7 @@ public class rkoch_assignment4_p2 {
 		} else { resultsFile2 = new PrintWriter(new File(filename1)); }
 	}
 
-	// handles the input being routed to the correct functions
+	// handles the input being routed to the correct functions for part 1
 	public static void part1InputHandler() throws IOException {
 
 		try {
@@ -82,6 +92,33 @@ public class rkoch_assignment4_p2 {
 			while(file.hasNextDouble()) {
 				planes.add(readPlaneFromFile(file));
 				points.add(readVectorFromFile(file));
+			}
+		}
+
+		// prints an error message if the input is not in the correct format
+		catch(NoSuchElementException e) {
+			failFast();
+		}
+	}
+
+	// handles the input being routed to the correct functions for part 2
+	public static void part2InputHandler() throws IOException {
+
+		try {
+			// open the file for reading
+			Scanner file = new Scanner(inputFile);
+
+			// create a line from the given points 
+			Vector point1 = readVectorFromFile(inputFile);
+			Vector point2 = readVectorFromFile(inputFile);
+			line = new Line(point1, point2.subtract(point1));
+
+			// throwaway the next 3 numbers by moving to the next line
+			file.NextLine();
+
+			// fill array lists with planes and points
+			while(file.hasNextDouble()) {
+				triangles.add(readTriangleFromFile(file));
 			}
 		}
 
@@ -111,6 +148,14 @@ public class rkoch_assignment4_p2 {
 		return new Plane(point, normal);
 	}
 
+	// reads in a triangle from a given file
+	public static Triangle readTriangleFromFile(Scanner file) throws IOException {
+		Vector vertex1 = readVectorFromFile(file);
+		Vector vertex2 = readVectorFromFile(file);
+		Vector vertex3 = readVectorFromFile(file);
+		return new Triangle(vertex1, vertex2, vertex3);
+	}
+
 	// rounds a double to a specified number of significant digits
 	public static double roundToSignificantDigits(double number, int digits) {
 		double roundedNum;
@@ -129,13 +174,6 @@ public class rkoch_assignment4_p2 {
 		resultsFile2.println("Not valid input.");
 		resultsFile2.close();
 		System.exit(1);
-	}
-
-	// prints a vector to a given file
-	public static void printVectorToFile(Vector vector, PrintWriter file) throws IOException {
-		for (int i = 0; i < 3; i++) {
-			file.printf("%-6.4g", roundToSignificantDigits(vector.coords[i], 4));
-		}
 	}
 
 	// prints a number to a given file
