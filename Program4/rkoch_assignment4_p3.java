a3eb875d4b77069ba3d98c3dd58bbd39e86bc83c
raytracer <ray3618@gmail.com>
2022-12-01 17:16:10 -0700

    added input and output file creation

---
+++ Program4/rkoch_assignment4_p3.java
@@ -0,0 +1,56 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.math.BigDecimal;
+import java.math.MathContext;
+
+public class rkoch_assignment4_p3 {
+
+	public static File inputFile;
+	public static PrintWriter resultsFile1;
+
+	public static void main(String[] args) throws IOException {
+
+		// setup the input and output files
+		String filename = null;
+		if(args.length > 0) filename = args[0];
+		setupTheInputFile(filename);
+		if(args.length > 1) filename = args[1];
+		setupTheOutputFile(filename);
+	}
+
+	// prompts the user for an input file and returns it
+	public static void setupTheInputFile(String filename) throws IOException {
+		
+		// if a filename was not entered, then prompt for one
+		if(filename == null) {
+			Scanner userInput = new Scanner(System.in);
+			System.out.printf("Please enter the name of an input file: ");
+			filename = userInput.nextLine();
+		}
+
+		inputFile = new File(filename);
+	}
+
+	// creates and sets up the output file for writing
+	public static void setupTheOutputFile(String filename) throws IOException {
+		
+		// if filenames were not entered on the command line, then prompt for them
+		if(filename == null) {
+			Scanner userInput = new Scanner(System.in);
+			System.out.printf("Please enter the name of an output file: ");
+			filename = userInput.nextLine();
+			resultsFile1 = new PrintWriter(new File(filename));
+		} else { resultsFile1 = new PrintWriter(new File(filename)); }
+	}
+
+	// prints a matrix to a given file
+	public static void printVectorToFile(Vector vector, PrintWriter file) throws IOException {
+		for (int i = 0; i < 3; i++) {
+			file.printf("%-8.4g", roundToSignificantDigits(vector.coords[i], 4));
+		}
+		file.println();
+	}
+}
