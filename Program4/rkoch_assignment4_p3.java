bed9c97201435ed052763b0c6f513626499a01eb
raytracer <ray3618@gmail.com>
2022-12-01 19:55:35 -0700

    read in the matrix form the input file

--- Program4/rkoch_assignment4_p3.java
+++ Program4/rkoch_assignment4_p3.java
@@ -8,8 +8,9 @@ import java.math.MathContext;
 
 public class rkoch_assignment4_p3 {
 
+	public static Matrix webPages;
 	public static File inputFile;
-	public static PrintWriter resultsFile1;
+	public static PrintWriter resultsFile;
 
 	public static void main(String[] args) throws IOException {
 
@@ -19,6 +20,10 @@ public class rkoch_assignment4_p3 {
 		setupTheInputFile(filename);
 		if(args.length > 1) filename = args[1];
 		setupTheOutputFile(filename);
+
+		readMatrixFromFile();
+
+		resultsFile.close();
 	}
 
 	// prompts the user for an input file and returns it
@@ -42,8 +47,37 @@ public class rkoch_assignment4_p3 {
 			Scanner userInput = new Scanner(System.in);
 			System.out.printf("Please enter the name of an output file: ");
 			filename = userInput.nextLine();
-			resultsFile1 = new PrintWriter(new File(filename));
-		} else { resultsFile1 = new PrintWriter(new File(filename)); }
+		} 
+
+		resultsFile = new PrintWriter(new File(filename));
+	}
+
+	// count the number of lines in the input file
+	public static int determineMatrixSize() throws IOException {
+		Scanner scanLines = new Scanner(inputFile);
+
+		int count = 0;
+		while (scanLines.hasNextLine()) {
+			scanLines.nextLine();
+			count++;
+		}
+		return count;
+	}
+
+	// reads the matrix values in from a file
+	public static void readMatrixFromFile() throws IOException {
+		int numLines = determineMatrixSize();
+		webPages = new Matrix(numLines, numLines);
+
+		// open the file for reading
+		Scanner file = new Scanner(inputFile);
+
+		for (int row = 0; row < webPages.rows; row++) {
+			for (int column = 0; column < webPages.columns; column++) {
+				webPages.setValue(row, column, file.nextDouble());
+			}
+		}
+
 	}
 
 	// rounds a double to a specified number of significant digits
@@ -58,10 +92,29 @@ public class rkoch_assignment4_p3 {
 	}
 
 	// prints a matrix to a given file
-	public static void printVectorToFile(Vector vector, PrintWriter file) throws IOException {
+	public static void printVectorToFile(Vector vector) throws IOException {
 		for (int i = 0; i < 3; i++) {
-			file.printf("%-8.4g", roundToSignificantDigits(vector.coords[i], 4));
+			resultsFile.printf("%-8.4g", roundToSignificantDigits(vector.coords[i], 4));
 		}
-		file.println();
+		resultsFile.println();
+	}
+
+	// causes the program to print a failure message and exit
+	public static void failFast() {
+		resultsFile.println("Invalid input.");
+		resultsFile.close();
+
+		System.exit(1);
+	}
+
+
+	// prints a matrix to a given file
+	public static void printMatrix(Matrix matrix) {
+		for (int row = 0; row < matrix.rows; row++) {
+			for (int column = 0; column < matrix.columns; column++) {
+				resultsFile.printf("%-8.4g", roundToSignificantDigits(matrix.getValue(row, column), 4));
+			}
+			resultsFile.println();
+		}
 	}
 }
